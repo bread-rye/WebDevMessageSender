8ceacfd07923c79695c7ffa1633599cc547c9c44
rye-bread <116127009+bread-rye@users.noreply.github.com>
2024-05-04 20:41:04 +0800

    Create index.py

---
+++ index.py
@@ -0,0 +1,36 @@
+import re
+import requests
+import time
+import argparse
+
+def decode(s):
+    return decodeURIComponent(re.sub("[0-9a-f]{2}", "%$&"))
+
+def callAPI(url, message, body):
+    requests.post(url, json= {
+        "method":"POST",
+        "headers": {"Content-Type": "application/json"},
+        "body": body | {"content":message}
+        })
+
+def getAPI(u, m):
+    greq = requests.get(u)
+    callAPI(u, m, greq.json())
+
+last_call = open("last_call.txt", "r+")
+t_i = float(last_call.readLine())
+now = time.time()
+timeElapsed = (now-t_i)/60
+
+parser = argparse.ArgumentParser(description = "Dead Man's Executable")
+parser.add_argument('url')
+parser.add_argument('message')
+args = parser.parse_args()
+url, message = args.url, args.message
+
+if timeElapsed>5:
+    getAPI(url, message)
+else:
+    last_call.seek(0)
+    last_call.write(now)
+last_call.close()
